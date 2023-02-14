7847bdcdee415757db2ef13c95073da6f5834d63
efilnukefesin <efil.threat@googlemail.com>
2023-02-14 20:50:43 +0100

    Added PluginHostProvider Tests

---
+++ src/PluginDemo.Tests/PluginHostProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using PluginDemo.Interfaces;
+using PluginDemo.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemo.Tests
+{
+    [TestClass]
+    public class PluginHostProviderTests
+    {
+        #region Creation
+        [TestMethod]
+        public void Creation()
+        {
+            IPluginHostProvider provider = new PluginHostProvider();
+
+            Assert.IsNotNull(provider);
+            Assert.IsTrue(provider.Plugins.Count > 0);
+        }
+        #endregion Creation
+    }
+}
